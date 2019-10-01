7bca1c040859393d4d813b77204eb25069211e1c
Mark Gil Libres <markglibres@gmail.com>
2019-10-01 22:20:49 +1000

    add unit test for getting all cars

---
+++ tests/BizCover.Api.Cars.Unit.Tests/Services/WhenGettingCars.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using BizCover.Repository.Cars;
+using Moq;
+using NUnit.Framework;
+
+namespace BizCover.Api.Cars.Unit.Tests.Services
+{
+    public class WhenGettingCars : GivenCarService
+    {
+        private List<Car> _cars;
+        private IEnumerable<Car> _result;
+
+        protected override void Arrange()
+        {
+            base.Arrange();
+
+            var vintageCar = Fixture.Create<Car>();
+            var cheapCar = Fixture.Create<Car>();
+            var luxuryCar = Fixture.Create<Car>();
+
+            _cars = new List<Car>
+            {
+                vintageCar,
+                cheapCar,
+                luxuryCar
+            };
+
+            MockRepository.Setup(r => r.GetAllCars())
+                .ReturnsAsync(_cars);
+        }
+
+        protected override async Task ActAsync()
+        {
+            await base.ActAsync();
+            _result = await CarService.GetCars(
+                _cars.First().Id,
+                _cars.Last().Id);
+        }
+
+        [Test]
+        public void Should_Return_Two_Cars()
+        {
+            Assert.AreEqual(2, _result.Count());
+        }
+
+        [Test]
+        public void Should_Match_Expected_Cars()
+        {
+            Assert.AreEqual(_cars.First()
+                    .Id,
+                _result.First()
+                    .Id);
+            Assert.AreEqual(_cars.Last()
+                    .Id,
+                _result.Last()
+                    .Id);
+        }
+    }
+}
