d07d0c86c1a095715a196f8b7b16d7e63c6efb05
Mark Gil Libres <markglibres@gmail.com>
2019-10-01 22:31:00 +1000

    add unit test for calculating discount

---
+++ tests/BizCover.Api.Cars.Unit.Tests/Services/WhenCalculatingDiscount.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using AutoFixture;
+using BizCover.Api.Cars.Application.Seedwork;
+using BizCover.Repository.Cars;
+using Moq;
+using NUnit.Framework;
+
+namespace BizCover.Api.Cars.Unit.Tests.Services
+{
+    public class WhenCalculatingDiscount : GivenCarService
+    {
+        private decimal _result;
+
+        protected override void Arrange()
+        {
+            var oneCalculator = Fixture.Create<Mock<ICarDiscountCalculator>>();
+            var twoCalculator = Fixture.Create<Mock<ICarDiscountCalculator>>();
+
+            oneCalculator.Setup(c => c.Calculate(It.IsAny<IEnumerable<Car>>()))
+                .Returns(50);
+            twoCalculator.Setup(c => c.Calculate(It.IsAny<IEnumerable<Car>>()))
+                .Returns(25);
+
+            Fixture.Register<IEnumerable<ICarDiscountCalculator>>(() => new List<ICarDiscountCalculator>
+                {oneCalculator.Object, twoCalculator.Object});
+
+            base.Arrange();
+        }
+
+        protected override void Act()
+        {
+            base.Act();
+            _result = CarService.CalculateDiscount(new List<Car>());
+        }
+
+        [Test]
+        public void Should_Match_Expected_Discount()
+        {
+            Assert.AreEqual(75, _result);
+        }
+    }
+}
